54c52d551ddde901bfd0e541b86cb5f0ec7dda76
George pan <panagiotakopoulosgeorge@yahoo.gr>
2020-04-28 16:24:35 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            string name=null;
+            string password;
+            bool autoSave=false;
+
+            Console.WriteLine("do you want to autosave ?(yes/no) ");
+            answer = Console.ReadLine().ToLower();
+            answer.Trim();
+            if (answer == "yes")
+            { autoSave = true;
+            }
+            Console.WriteLine("what is yr name?");
+            if (autoSave = true)
+            {
+                name = Console.ReadLine().Trim();
+            }
+            else
+            {
+                Console.ReadLine();
+            }
+            Console.WriteLine(name);
+        }
+    }
+}
